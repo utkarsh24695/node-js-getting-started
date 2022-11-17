9fbe9b63332283f9ad770e5f619508bd52d066b0
utkarsh24695 <utkarsh24695@hotmail.com>
2022-11-17 18:26:35 +0530

    creating error

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.envPORT || 5000
+const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -8,3 +8,10 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+  gfwejken
+  wfegcjkaes
+  eahgejfkwew
+  wjeafkelar
+  faweFJewbakf
+  
