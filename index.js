dc6f9e48b1efb3767afe043c95f1e2402c476ce6
utkarsh24695 <utkarsh24695@hotmail.com>
2022-11-17 18:29:36 +0530

    errors corrected

--- index.js
+++ index.js
@@ -8,10 +8,3 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-  gfwejken
-  wfegcjkaes
-  eahgejfkwew
-  wjeafkelar
-  faweFJewbakf
-  
